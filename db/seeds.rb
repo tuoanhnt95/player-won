56cba1fd14db1c775cab78261e3a42b5be0b97a7
mengrui song <summerrock1017@gmail.com>
2022-08-16 14:18:21 +0900

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,21 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 require "open-uri"
-puts "Destory the database..."
+require "faker"
+
+puts "Destory the booking database..."
+Booking.destroy_all
+
+puts "Destory the offer database..."
+Offer.destroy_all
+
+puts "Destory the user database..."
+User.destroy_all
+
+puts "Destory the game database..."
 Game.destroy_all
+
+# 39073 pages of games are there
 pages = (1..3).to_a
 pages.each do |num|
   url = "https://api.rawg.io/api/games?page=#{num}&key=b7a4871ebfad430ca835d6587cc0c026"
@@ -20,4 +33,55 @@ pages.each do |num|
     )
   end
 end
-puts "#{Game.count} games are created"
+puts "#{Game.count} games are created:)"
+
+
+
+user_names = ['Oanh', 'Song', 'Tony', 'Yaya']
+user_names.each do |user_name|
+  User.create!(
+    name: user_name,
+    email: Faker::Internet.unique.email,
+    password: [*('a'..'z'),*('0'..'9')].shuffle[0,8].join
+  )
+end
+puts "#{User.count} users are created:)"
+
+
+
+titles = ["Good Coach", "Do you want to be perfect at Xgame", "Perfect Coach"]
+
+titles.each_with_index do |title, index|
+  Offer.create!(
+    title: title,
+    duration: rand(0.5...5.0).round(1),
+    price: rand(1000...100000),
+    game: Game.all[index],
+    user: User.all[index]
+  )
+end
+puts "#{Offer.count} offers are created:)"
+
+
+
+
+Booking.create!(
+  date: Time.now - 1.day,
+  status: 0,
+  offer: Offer.all[0],
+  user: User.all[3]
+)
+Booking.create!(
+  date: Time.now - 3.day,
+  status: 1,
+  offer: Offer.all[1],
+  user: User.all[0]
+)
+Booking.create!(
+  date: Time.now - 5.day,
+  status: 2,
+  offer: Offer.all[2],
+  user: User.all[3]
+)
+
+puts "#{Booking.count} bookings are created:)"
