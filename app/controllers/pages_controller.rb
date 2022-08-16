ee70fab7543e1dda2bb0777254a5b375e388ecdf
Oanh Nguyen <myspamvn@yahoo.com>
2022-08-17 01:20:08 +0900

    created booking index, booking create, booking destroy, offer create in sdashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,9 @@ class PagesController < ApplicationController
   end
 
   def dashboard
-    # @offers = Offer.where()
-    # @bookings = Booking.where()
+    @have_bookings = policy_scope(Booking)
+    @make_bookings = policy_scope(Booking).where(user: current_user)
+    @offers = policy_scope(Offer).where(user: current_user)
+    # select bookings of offers where the person who creates the offers is you
   end
-
 end
