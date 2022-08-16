26f6ce14abe4d4996f90b1a2ba876902d98f928d
mengrui song <summerrock1017@gmail.com>
2022-08-16 19:07:09 +0900

    fixed routes and test the booking new, add booking form in offer-show

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "offers#index"
 
-  resources :offers, only: [:index, :show, :new, :create, :Destory] do
-    resources :bookings, only: [:new, :create]
+  resources :offers, only: [:index, :show, :new, :create, :destroy]   do
+    resources :bookings, only: [:create]
   end
   resources :bookings, only: [:destroy]
 
