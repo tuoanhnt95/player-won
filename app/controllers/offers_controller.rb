0bb313a5c5ef96d623453f20874e39a276dadd73
Tony Leng <lengtk@gmail.com>
2022-08-17 12:30:09 +0900

    finished offers show page

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -8,5 +8,32 @@ class OffersController < ApplicationController
   def show
     @offer = Offer.find(params[:id])
     authorize @offer
+    @booking = Booking.new
+  end
+
+  def new
+    # this instance is for the form
+    @offer = Offer.new
+    authorize @offer
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+    @offer.user = current_user
+    authorize @offer
+    if @offer.save
+      redirect_to dashboard_path
+    else
+      # render 'new.html.erb'
+      # redirect_to new_restaurant_path
+      render :new
+    end
+  end
+
+  private
+
+  # strong params -> whitelisting the attributes that the user can give us
+  def offer_params
+    params.require(:offer).permit(:title, :description, :duration, :price, :game_id)
   end
 end
